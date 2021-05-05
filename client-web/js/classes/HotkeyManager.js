7942fe507815c406cfe7ab733d3c526ceda7c3c7
onikitina <onikitina@voximplant.com>
2021-05-05 19:32:10 +0300

    changed hotkeys Alt(Option for Mac) + E for camera toggle, Alt(Option for Mac) + D for mic toggle

--- client-web/js/classes/HotkeyManager.js
+++ client-web/js/classes/HotkeyManager.js
@@ -2,27 +2,15 @@ let callInterface;
 function registerCallbacks(ci) {
   callInterface = ci;
   window.addEventListener('keydown', keyDownToggle);
-  window.addEventListener('keyup', keyUpToggle);
 }
 
 function unregisterCallback() {
   window.removeEventListener('keydown', keyDownToggle);
-  window.removeEventListener('keyup', keyUpToggle);
 }
-
-const actKeys = ['KeyD', 'KeyE'];
-
-let pressedKey = '';
 
 function keyDownToggle(ev) {
-  if (pressedKey) return;
-  if (actKeys.includes(ev.code)) pressedKey = ev.code;
-}
-function keyUpToggle(ev) {
-  if (!pressedKey) return;
-  if (actKeys.includes(ev.code)) pressedKey = null;
-  if (ev.code === 'KeyD' && ev.ctrlKey === true) callInterface.muteToggle();
-  if (ev.code === 'KeyE' && ev.ctrlKey === true) callInterface.cameraToggle();
+  if (ev.code === 'KeyD' && ev.altKey) callInterface.muteToggle();
+  if (ev.code === 'KeyE' && ev.altKey) callInterface.cameraToggle();
 }
 
 export { registerCallbacks, unregisterCallback };
