6dbe9f44dfa30c827aea6c22092a260f77ce3758
Olga Nikitina <52132940+Olya-Nik@users.noreply.github.com>
2021-05-06 17:38:02 +0300

    Hotkeys (#5)
    Keys M, V serve as hotkeys if pressed outside of input element

--- client-web/js/classes/HotkeyManager.js
+++ client-web/js/classes/HotkeyManager.js
@@ -9,8 +9,17 @@ function unregisterCallback() {
 }
 
 function keyDownToggle(ev) {
-  if (ev.code === 'KeyD' && ev.altKey) callInterface.muteToggle();
-  if (ev.code === 'KeyE' && ev.altKey) callInterface.cameraToggle();
+  if (pressedKey) return;
+  if (actKeys.includes(ev.code)) pressedKey = ev.code;
+  if (ev.code === 'Space' && ev.target.nodeName !== 'INPUT') callInterface.muteToggle();
+}
+function keyUpToggle(ev) {
+  if (!pressedKey) return;
+  if (actKeys.includes(ev.code)) pressedKey = null;
+  if ((ev.code === 'Space' || ev.code === 'KeyM') && ev.target.nodeName !== 'INPUT')
+    callInterface.muteToggle();
+  if (ev.code === 'KeyV' && ev.ctrlKey === false && ev.target.nodeName !== 'INPUT')
+    callInterface.cameraToggle();
 }
 
 export { registerCallbacks, unregisterCallback };
